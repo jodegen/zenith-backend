4accc3a0de2add7654fea51173308d03f6d34080
jodegen <jonasdegen01@gmail.com>
2026-03-09 20:57:32 +0100

    Initial Commit

---
+++ src/main/java/dev/zenith/finance/zenith_backend/ZenithBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.zenith.finance.zenith_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenithBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenithBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zenith/finance/zenith_backend/ZenithBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zenith.finance.zenith_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenithBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
